fd199ed377ba2feb1164f4b9583d2d0e264fc58e
Chester Zara <chesterjzara@gmail.com>
2019-03-11 14:21:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -35,20 +35,32 @@ var checkForMatch = function() {
     }
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
     console.log("User flipped "+ cards[cardId].rank);
     console.log("It was a "+ cards[cardId].suit);
     console.log("Img in "+ cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
     if(cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.querySelector('#game-board').appendChild(cardElement);
+    }
+}
 
 
+//flipCard(0);
+//flipCard(2);
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 //git add -A (or check status and specify files)
 //git commit -m 'Added object array'
