06831670e90df042e1be39d6f3a23f59ff567b78
Chester Zara <chesterjzara@gmail.com>
2019-03-11 14:48:30 -0500

    Added Reset button and score tracking

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,16 +25,19 @@ var cards = [
 ]
 
 var cardsInPlay = [];
+var playerWins = 0;
+var playerGames = 0;
 
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
+        playerWins ++;
     }
     else {
         alert("Sorry bud, try again.");
     }
+    playerGames++;
 }
-
 var flipCard = function () {
     var cardId = this.getAttribute('data-id');
     console.log("User flipped "+ cards[cardId].rank);
@@ -45,6 +48,7 @@ var flipCard = function () {
     if(cardsInPlay.length === 2) {
         checkForMatch();
     }
+    updateScore();
 }
 var createBoard = function() {
     for (var i = 0; i < cards.length; i++) {
@@ -52,15 +56,34 @@ var createBoard = function() {
         cardElement.setAttribute('src', "images/back.png");
         cardElement.setAttribute('data-id', i);
         cardElement.addEventListener('click', flipCard);
+        cardElement.className ='card-image';
         document.querySelector('#game-board').appendChild(cardElement);
     }
 }
-
-
-//flipCard(0);
-//flipCard(2);
+var resetBoard = function() {
+    console.log("Reset triggered....");
+    var cardsToReset = document.getElementsByClassName('card-image');
+    cardsInPlay = []; 
+    for (var i = 0; i < cards.length; i++) {
+        cardsToReset[i].setAttribute('src', "images/back.png");
+    }
+}
+var updateScore = function() {
+    console.log("Wins:" + playerWins + " | Games: " + playerGames);
+    document.getElementById('wins').textContent = playerWins;
+    document.getElementById('loses').textContent = playerGames - playerWins;
+}
 
 createBoard();
+
+var resetButton = document.querySelector('button');
+resetButton.addEventListener('click', resetBoard);
+
+
+
+
+
+
 
 //git add -A (or check status and specify files)
 //git commit -m 'Added object array'
